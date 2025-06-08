77fda014a92038283bdba616692d9ef2f1a0691c
liz <angeladeliz@uniplaclages.edu.br>
2025-06-08 19:02:28 -0300

    criado serviço de funções

---
+++ MicroserviceUsuario/ServiceFuncoes.py
@@ -0,0 +1,3 @@
+#IMPLEMENTE AQUI A CLASSE DE FUNÇÕES
+#implemente um classe de validação de email
+# Ajudinha: expressão regular para validar um e-mail r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
